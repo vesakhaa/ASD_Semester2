dc06760664ef973db0b05703bd087e953b7bb73f
vesakhaa <anandavesakha2905@gmail.com>
2026-02-10 22:26:16 +0700

    first commit

---
+++ JOBSHEET 1/perulangan.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class perulangan {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan NIM anda: ");
+        long nim = input.nextLong();
+
+        //ambil 2 digit terakhir NIM
+        int n = (int)(nim % 100);
+
+        if (n<10){
+            n = n + 10;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if(i==10 || i==15){
+                continue;
+            }
+            if(i  % 3 == 0){
+                System.out.print("# ");
+            }else if (i % 2 == 0){
+                System.out.print(i + " ");
+            }else {
+                System.out.print("* ");
+            }
+
+        }
+
+        input.close();
+    }
+}
