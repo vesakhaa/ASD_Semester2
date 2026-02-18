ca20e94044ca18b8c392cf214e3045d08e3b7584
vesakhaa <anandavesakha2905@gmail.com>
2026-02-18 10:15:48 +0700

    first commit

---
+++ JOBSHEET 2/LAT1/Mahasiswa04.java
@@ -0,0 +1,34 @@
+class Mahasiswa04{
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+    
+
+    void tampilkanInformasi(){
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+
+    void ubahKelas(String kelasBaru){
+        kelas = kelasBaru;
+    }
+
+    void updateIPK(double ipkBaru){
+        ipk = ipkBaru;
+    }
+
+    String nilaiKinerja(){
+        if (ipk >= 3.5) {
+            return "Kinerja Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja Baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja Cukup";
+        } else {
+            return "Kinerja Kurang";
+        }
+    }
+}
