02b27efc9182d6931582a65dd39837e8d648aa8c
vesakhaa <anandavesakha2905@gmail.com>
2026-02-18 10:20:08 +0700

    second commit (modifikasi validasi IPK)

--- JOBSHEET 2/LAT1/Mahasiswa04.java
+++ JOBSHEET 2/LAT1/Mahasiswa04.java
@@ -21,6 +21,9 @@ class Mahasiswa04{
     }
 
     String nilaiKinerja(){
+        if(ipk <= 0 || ipk >= 4.0){
+            return "IPK tidak valid. Harus antara 0.0 dan 4.0";
+        }
         if (ipk >= 3.5) {
             return "Kinerja Sangat Baik";
         } else if (ipk >= 3.0) {
