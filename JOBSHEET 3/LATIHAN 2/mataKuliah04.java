f3cce0d953bf6bf99d317a3dcd053e92639b9387
vesakhaa <anandavesakha2905@gmail.com>
2026-02-24 21:57:28 +0700

    First Commit

---
+++ JOBSHEET 3/LATIHAN 2/mataKuliah04.java
@@ -0,0 +1,13 @@
+public class mataKuliah04 {
+    public String  kode;
+    public String  nama;
+    public int sks;
+    public int jumlahJam;
+
+    public mataKuliah04(String kode, String nama, int sks, int jumlahJam){
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
