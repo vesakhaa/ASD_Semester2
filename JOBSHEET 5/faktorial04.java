0bd1c4d536d9614a8316915a7172d9dafd25e3da
vesakhaa <anandavesakha2905@gmail.com>
2026-03-14 12:20:32 +0700

    modif perulangan

--- JOBSHEET 5/faktorial04.java
+++ JOBSHEET 5/faktorial04.java
@@ -1,17 +1,21 @@
+
 public class faktorial04 {
-    int faktorialBF(int n){
+
+    int faktorialBF(int n) {
         int fakto = 1;
-        for (int i = 1; i <= n; i++) {
-            fakto = fakto*i;
+        int i = 1;
+        while (i <= n) {
+            fakto = fakto * i;
+            i++;
         }
         return fakto;
     }
 
-    int faktorialDC(int n){
-        if (n==1) {
+    int faktorialDC(int n) {
+        if (n == 1) {
             return 1;
         } else {
-            int fakto = n * faktorialDC(n-1);
+            int fakto = n * faktorialDC(n - 1);
             return fakto;
         }
     }
