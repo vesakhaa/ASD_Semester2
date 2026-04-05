60e98a6df7596d9640113b3128a37d097463ae8e
vesakhaa <anandavesakha2905@gmail.com>
2026-04-05 19:05:49 +0700

    SECOND COMMIT

--- JOBSHEET 5/mahasiswaDemo04.java
+++ JOBSHEET 5/mahasiswaDemo04.java
@@ -1,28 +1,48 @@
+import java.util.Scanner;
+
 public class mahasiswaDemo04 {
 
-    public static void main(String[] args) {
-    mahasiswaBerprestasi list = new mahasiswaBerprestasi();
+    // Method main ditambahkan kembali di sini
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); 
+        System.out.print("Masukkan jumlah mahasiswa yang akan diinput: ");
+        int jumlahMhs = sc.nextInt();
 
-    mahasiswa04 m1 = new mahasiswa04("123", "Zidan","2A", 3.2);
-    mahasiswa04 m2 = new mahasiswa04("124", "Ayu", "2A", 3.5);
-    mahasiswa04 m3 = new mahasiswa04("125", "Sofi", "2A", 3.1);
-    mahasiswa04 m4 = new mahasiswa04("126", "Sita", "2A", 3.9);
-    mahasiswa04 m5 = new mahasiswa04("127", "Miki", "2A", 3.7);
+        mahasiswaBerprestasi list = new mahasiswaBerprestasi(jumlahMhs);
 
-    list.tambah(m1);
-    list.tambah(m2);
-    list.tambah(m3);
-    list.tambah(m4);
-    list.tambah(m5);
 
-    System.out.println("Data mahasiswa sebelum sorting: ");
-    list.tampil();
+        for (int i = 0; i < jumlahMhs; i++) {
+            System.out.println("\n--- Masukkan data mahasiswa ke-" + (i + 1) + " ---");
+            System.out.print("NIM   : ");
+            String nim = input.nextLine();
+            
+            System.out.print("Nama  : ");
+            String nama = input.nextLine();
+            
+            System.out.print("Kelas : ");
+            String kelas = input.nextLine();
+            
+            System.out.print("IPK   : ");
+            double ipk = sc.nextDouble();
 
-    System.out.println("\n");
+            mahasiswa04 m = new mahasiswa04(nim, nama, kelas, ipk);
 
-    System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC): ");
-    list.bubbleSort();
-    list.tampil();
+            list.tambah(m);
+        }
 
-    }
-}
+        System.out.println("\n----------------------------------------------");
+        System.out.println("Data mahasiswa sebelum sorting: ");
+        list.tampil();
+
+        System.out.println("\n----------------------------------------------");
+        System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC): ");
+        list.bubbleSort();
+        list.tampil();
+
+        System.out.println("\n----------------------------------------------");
+        System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC):");
+        list.selectionSort();
+        list.tampil();
+    } 
+} 
